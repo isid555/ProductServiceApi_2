e7a58a512b360b206a7a4926669549b6235614c5
isid555 <r555sid@gmail.com>
2024-04-11 13:41:20 +0530

    Exception_Handlers

--- src/main/java/com/example/product/Exception2/PageNotFound.java
+++ src/main/java/com/example/product/Exception2/PageNotFound.java
@@ -3,7 +3,7 @@ package com.example.product.Exception2;
 public class PageNotFound extends RuntimeException{
 //   private Long id;
 //   private String message;
-   public PageNotFound(Long id,String message){
+   public PageNotFound(String message){
        super(message);
 
    }
